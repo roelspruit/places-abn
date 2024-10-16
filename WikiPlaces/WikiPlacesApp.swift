7afebc5b8296f592a087f641b7f5baf26db02c8e
Roel Spruit <rspruit@anwb.nl>
2024-10-16 16:54:51 +0200

    Initial Commit

---
+++ WikiPlaces/WikiPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiPlacesApp.swift
+//  WikiPlaces
+//
+//  Created by Roel Spruit on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WikiPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
